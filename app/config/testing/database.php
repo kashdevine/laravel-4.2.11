cdfbc77ff2a50fa3d3a986246d3d295626cb7e02
kashdevine <kasheen.mowatt@gmail.com>
2021-11-11 17:11:03 -0800

    set database config locally

--- app/config/testing/database.php
+++ app/config/testing/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'learninglaravel',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'Shante4794!@#$',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
